c8110832bf564c1c7439ab061abd768dab3b631d
twinne66 <twinne66@gmail.com>
2024-02-08 00:45:48 +0900

    curriculum8-2correct

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-    return view('posts.index')->with(['posts' => $post->getByLimit()]);
+    return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
     
     public function show(post $post)
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,7 +9,7 @@ class Post extends Model
 {
     use HasFactory;
     
-public function getByLimit(int $limit_count = 10)
+public function getPaginateByLimit(int $limit_count = 10)
 {
   return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
 }}
