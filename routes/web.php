f7efc21a43368ec89eb017846df250b05e01faa0
twinne66 <twinne66@gmail.com>
2024-02-14 21:37:13 +0900

    カリキュラム課題4

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('posts/{post}' , [PostController::class, 'show']);
 Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
 Route::put('/posts/{post}', [PostController::class, 'update']);
-
+Route::delete('/posts/{post}', [Postcontroller::class, 'delete']);
